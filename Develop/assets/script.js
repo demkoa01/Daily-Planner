5ce9631a03bd7bd1bbbd2e034d82edf2e59dde02
Allison Demkovich <demkovich.allison@gmail.com>
2022-01-08 21:45:06 -0600

    reograngize HTML & JS so each timeblock doesn't need it's own save function in JS. Get localstorage to save on reload of page

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,120 +1,65 @@
 // variables
-var timeNow;
-var hour;
-var timer;
-const hour1 = $('#time1').text();
-const hour2 = $('#time2').text();
-const hour3 = $('#time3').text();
-const hour4 = $('#time4').text();
-const hour5 = $('#time5').text();
-const hour6 = $('#time6').text();
-const hour7 = $('#time7').text();
-const hour8 = $('#time8').text();
-const hour9 = $('#time9').text();
-
-// display current date at top of page
-var date = new Date();
-var todaysDate = date.toDateString();
-var displayDate = document.querySelector("#currentDay");
-displayDate.append(todaysDate);
+var hour8 = $("#8");
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#1");
+var hour2 = $("#2");
+var hour3 = $("#3");
+var hour4 = $("#4");
+var hour5 = $("#5");
+var time = moment();
 
 // get current time 
 function currentHour() {
-    var timeNow = date.getHours();
-    // check for time in miltitary time
-    console.log(timeNow);
-    
-    var amPM = "";
-    
-    // convert time to 12HR 
-    if (timeNow > 12){
-        var hour = timeNow -12;
-        suffix = "PM";
-    }
-    else {
-        var hour = timeNow;
-        suffix = "AM";
-    }
-    console.log(hour + suffix);
-    return hour + suffix;
+    // set current date at the top header
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    // load timeblock description textarea with what was in local storage
+    $(".time-block").each(function() {
+        var id = $(this).attr("id");
+        var description = localStorage.getItem(id);
+
+        if (description !== null) {
+            $(this).children(".description").val(description);
+        }
+    });
 }
 currentHour();
 
-// create function to color time blocks based on current time
-// time for planner will be between 8:00-18:00
-function colorCodeTime() {
-    timer = setInterval(colorCodeTime, 1000);
-    
-    // if the time now is between working hours, defined as 8AM-6PM
-    if (timeNow >=8 && timeNow <=20){
-        for (var i=1; i<=13; i++) {
-            let hourBlock = parseInt($('#time'+i).text());
-           
-            // if the hourblock & time now are equal, use css style for 'present'
-            if (hourBlock == timeNow) {
-                $('#todo'+i).css('present');
-                continue;
-            }
-            // if hourblock is less than the time now, use css style for 'past'
-            if (hourBlock < timeNow) {
-                $('#todo'+i).css('past');
-            }
-            // if the hourblock is greater than the time now, use css style for 'future'
-            else {
-                $('#todo'+i).css('future');
-            }
-        }
-    }
-
-    // if time now is outside of working hours, defualt blocks to be a different color
-    else {
-        clearInterval(timer);
-        $('textarea').css('background-color', 'pink');
-    }
-};
 
 // store input to local storage if a 'Save Button' is clicked
-$('#saveBtn1').click(function(){
-    const text1 = document.getElementById("todo1").value;
-    const textString1 = JSON.stringify(text1);
-    localStorage.setItem("text1", textString1);
-   
-    if(localStorage == null) {
-    $window.localStorage.setItem("text1", textString1);
-    }
+var saveBtn = $(".saveBtn");
+
+// upon clicking a save button, the description text area should update with what the user just typed
+saveBtn.on("click", function(){
+    var time = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, description);
 });
 
-$('#saveBtn2').click(function(){
-    const text2 = document.getElementById("todo2").value;
-    const textString2 = JSON.stringify(text2);
-    localStorage.setItem("text2", textString2);
-   
-    if(localStorage == null) {
-    $window.localStorage.setItem("text2", textString2);
-    }
-});
+// create function to color time blocks based on current time
+// time for planner will be between 8:00-17:00
+function colorCodeTime() {
+    hour = time.hours();
+    $(".time-block").each(function(){
+        var timeNow = parseInt($(this).attr("id"));
 
-$('#saveBtn3').click(function(){
-    const text3 = document.getElementById("todo3").value;
-    const textString3 = JSON.stringify(text3);
-    localStorage.setItem("text3", textString3);
-   
-    if(localStorage == null) {
-    $window.localStorage.setItem("text3", textString3);
-    }
-});
-
-$('#saveBtn4').click(function(){
-    const text4 = document.getElementById("todo4").value;
-    const textString4 = JSON.stringify(text3);
-    localStorage.setItem("text4", textString4);
-   
-    if(localStorage == null) {
-    $window.localStorage.setItem("text4", textString4);
-    }
-});
-
-
-
+        // color timeblocks green if in the future
+        if (timeNow > hour) {
+            $(this).addClass("future");
+        }
+        // color timeblock red if it is the current hour
+        else if (timeNow === hour) {
+            $(this).addClass("present");
+        }
+        // color timeblock gray if the hour had already past
+        else {
+            $(this).addClass("past");
+        }
+    });
+};
 
 colorCodeTime();
