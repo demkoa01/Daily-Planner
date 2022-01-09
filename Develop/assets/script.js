6d5e3097fc925b89029ba67603c344af99b4cda4
Allison Demkovich <demkovich.allison@gmail.com>
2022-01-08 20:42:42 -0600

    update so local storage saves when user types, still need to save when refreshed

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,5 +1,16 @@
-var storedBlocks = [];
-var storedBlocks_NAME = "Stored Blocks";
+// variables
+var timeNow;
+var hour;
+var timer;
+const hour1 = $('#time1').text();
+const hour2 = $('#time2').text();
+const hour3 = $('#time3').text();
+const hour4 = $('#time4').text();
+const hour5 = $('#time5').text();
+const hour6 = $('#time6').text();
+const hour7 = $('#time7').text();
+const hour8 = $('#time8').text();
+const hour9 = $('#time9').text();
 
 // display current date at top of page
 var date = new Date();
@@ -8,30 +19,49 @@ var displayDate = document.querySelector("#currentDay");
 displayDate.append(todaysDate);
 
 // get current time 
-var timeNow = date.getHours();
-console.log(timeNow);
+function currentHour() {
+    var timeNow = date.getHours();
+    // check for time in miltitary time
+    console.log(timeNow);
+    
+    var amPM = "";
+    
+    // convert time to 12HR 
+    if (timeNow > 12){
+        var hour = timeNow -12;
+        suffix = "PM";
+    }
+    else {
+        var hour = timeNow;
+        suffix = "AM";
+    }
+    console.log(hour + suffix);
+    return hour + suffix;
+}
+currentHour();
 
 // create function to color time blocks based on current time
 // time for planner will be between 8:00-18:00
 function colorCodeTime() {
     timer = setInterval(colorCodeTime, 1000);
-
+    
     // if the time now is between working hours, defined as 8AM-6PM
-    if (timeNow >=8 && timeNow <=18){
-        for (var i=1; i<=8; i++) {
+    if (timeNow >=8 && timeNow <=20){
+        for (var i=1; i<=13; i++) {
             let hourBlock = parseInt($('#time'+i).text());
-
+           
             // if the hourblock & time now are equal, use css style for 'present'
-            if (hourBlock === timeNow) {
-                $('#text'+i).css('present');
+            if (hourBlock == timeNow) {
+                $('#todo'+i).css('present');
+                continue;
             }
             // if hourblock is less than the time now, use css style for 'past'
             if (hourBlock < timeNow) {
-                $('#text'+i).css('past');
+                $('#todo'+i).css('past');
             }
             // if the hourblock is greater than the time now, use css style for 'future'
             else {
-                $('#text'+i).css('future');
+                $('#todo'+i).css('future');
             }
         }
     }
@@ -39,77 +69,52 @@ function colorCodeTime() {
     // if time now is outside of working hours, defualt blocks to be a different color
     else {
         clearInterval(timer);
-        $('textarea').css('background-color', '#fafad2');
+        $('textarea').css('background-color', 'pink');
     }
 };
 
-// variables
-const hour8 = $('#8-am').text();
-const hour9 = $('#9-am').text();
-const hour10 = $('#10-am').text();
-const hour11 = $('#11-am').text();
-const hour12 = $('#12-pm').text();
-const hour1 = $('#1-pm').text();
-const hour2 = $('#2-pm').text();
-const hour3 = $('#3-pm').text();
-const hour4 = $('#4-pm').text();
-const hour5 = $('#5-pm').text();
-const hour6 = $('#6-pm').text();
-
-const hour8Input = parseInt(hour8);
-
-// // store input to local storage
-// $('#saveBtn8').click(function(){
-//     if($('#todo8').val()) {
-//         localStorage.removeItem('todo8');
-//     }
-
-//     localStorage.setItem("todo", JSON.stringify(hour8));
-//     console.log(hour8String);
-//     console.log(localStorage.setItem("todo", JSON.stringify(hour8)))
-// });
-
-// edit stored time blocks
-function editBlocks(pText, pID) {
-    nBlock = {
-        id: pID,
-        input: pText.trim()
+// store input to local storage if a 'Save Button' is clicked
+$('#saveBtn1').click(function(){
+    const text1 = document.getElementById("todo1").value;
+    const textString1 = JSON.stringify(text1);
+    localStorage.setItem("text1", textString1);
+   
+    if(localStorage == null) {
+    $window.localStorage.setItem("text1", textString1);
     }
+});
 
-    for (var i=0; i<storedBlocks.length; i++) {
-        if(storedBlocks[i].id === nBlock.id) {
-            storedBlocks.splice(i,1);
-            localStorage.setItem(storedBlocks_NAME, JSON.stringify(storedBlocks));
-            return null;
-        }
+$('#saveBtn2').click(function(){
+    const text2 = document.getElementById("todo2").value;
+    const textString2 = JSON.stringify(text2);
+    localStorage.setItem("text2", textString2);
+   
+    if(localStorage == null) {
+    $window.localStorage.setItem("text2", textString2);
     }
-    storedBlocks.push(nBlock);
-    localStorage.setItem(storedBlocks_NAME, JSON.stringify(storedBlocks));
-};
+});
 
-function getStoredBlockTxt() {
-    if(localStorage.getItem(storedBlocks_NAME)) {
-        storedBlocks = JSON.parse(localStorage.getItem(storedBlocks_NAME));
-        storedBlocks.forEach(iBlock => {
-            iID = "#" + iBlock.id;
-            $iBlock = $(document.getElementById(iBlock.id));
-            $iBlock.val(iBlock.innput);
-
-            
-        });
+$('#saveBtn3').click(function(){
+    const text3 = document.getElementById("todo3").value;
+    const textString3 = JSON.stringify(text3);
+    localStorage.setItem("text3", textString3);
+   
+    if(localStorage == null) {
+    $window.localStorage.setItem("text3", textString3);
     }
-};
+});
+
+$('#saveBtn4').click(function(){
+    const text4 = document.getElementById("todo4").value;
+    const textString4 = JSON.stringify(text3);
+    localStorage.setItem("text4", textString4);
+   
+    if(localStorage == null) {
+    $window.localStorage.setItem("text4", textString4);
+    }
+});
+
+
 
 
 colorCodeTime();
-getStoredBlockTxt();
-
-// when the save button is clicked, it should trigger to update/save the updated input
-$(".saveBtn").click(function(){
-    console.log("save clicked");
-    $(this).toggleClass('unlocked');
-    $iTextArea = $($(this).parent().children()[1]);
-    iInput= document.getElementsByClassName("textarea").val(text);
-    iID = document.getElementsByClassName("textarea").attr("id");
-    editBlocks(iInput, iID);
-});
